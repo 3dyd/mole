ab6f0b8a25d7fe047260a856ad1811bba5c171f5
Mikalai Ananenka <git@3dyd.com>
2019-09-04 20:32:06 +0300

    fix character encoding problem in /defer_for

--- foo_mole/cli_defer_for.cpp
+++ foo_mole/cli_defer_for.cpp
@@ -14,12 +14,18 @@ public:
 
     pfc::string8 commands;
     base64_decode_to_string(commands, NextArg().c_str());
+    pfc::stringcvt::string_wide_from_utf8 commands_wide(commands, commands.get_length());
+    auto cmdline = std::wstring(L"foobar2000.exe ").append(commands_wide);
 
     ArgsProcessed();
 
-    std::thread deferred([cmdline{std::string("foobar2000.exe ").append(commands)}, delay]() {
+    std::thread deferred([cmdline{std::move(cmdline)}, delay]() {
       std::this_thread::sleep_for(delay);
-      system(cmdline.c_str());
+
+      if (0 != _wsystem(cmdline.c_str())) {
+        auto code = errno;
+        mlog << "failed to execute foobar2000.exe while running /defer_for command (errno=" << code << ")";
+      }
     });
 
     deferred.detach();
